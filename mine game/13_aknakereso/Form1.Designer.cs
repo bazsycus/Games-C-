646470bde51316c4c618a8a3bfee30a1c847641c
bazsycus <62937986+bazsycus@users.noreply.github.com>
2021-02-15 17:03:36 +0100

    Add files via upload

---
+++ mine game/13_aknakereso/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace _13_aknakereso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Oszlopszám";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(574, 15);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 28);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Könnyű";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(725, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Új játék";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sorszám";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(318, 39);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 29);
+            this.textBox2.TabIndex = 4;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(574, 40);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 28);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "Közepes";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(574, 67);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(6);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(84, 28);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.Text = "Nehéz";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(909, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Akna szám: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(909, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Zászló";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(995, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1027, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 731);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ mine game/13_aknakereso/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_aknakereso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //deklaráció
+        //integer
+        int iSor, iOszlop; //hány sor és oszlop van
+        int iKsor, iKoszlop;//klikkel sor, oszlop
+        int iMezo, iAkna;//metők száma, aknák száma
+        int iZaszlo; //lent levő zászló szám
+        int iFelforditott;
+        //bool
+        bool bStart = false; //elindult-e már a játék
+        bool bVege = false;
+        //tollak
+        Pen tollFeketeV = new Pen(Color.Black, 2);
+        Pen tollFekete = new Pen(Color.Black, 1);
+        
+        //ecset
+        SolidBrush ecsetFeher = new SolidBrush(Color.White);
+        SolidBrush ecsetAkna = new SolidBrush(Color.Red);
+        SolidBrush ecsetBetu = new SolidBrush(Color.DarkGreen);//ezzel írjuk kia betűket
+        //Tömbök
+        int[,] ia2Akna, ia2Klikk;
+        //Random
+        Random rnd = new Random();
+        //Fonr
+        Font fontKiiras = new Font("Comic Sans MS", 10, FontStyle.Bold);
+        /* [sor, oszlop]
+         * Akna-->  -1->>-1  akna hány szomszédon van akna
+         * Klikk 0->nincs felfedve, 1 bal klikkel van felfedve, -1 zászló
+         * 
+         */
+        private void JatekKezdet()
+        {
+            iFelforditott = 0;
+            iZaszlo = 0;
+            label5.Text = iZaszlo.ToString();
+            bVege = false;
+            iSor = int.Parse(textBox2.Text);
+            iOszlop = int.Parse(textBox1.Text);
+            iSor=Math.Min(20, Math.Max(5, iSor));
+            iOszlop = Math.Min(20, Math.Max(5, iOszlop));
+            bStart = true;
+            ia2Akna = new int[iSor+2, iOszlop+2];
+            ia2Klikk = new int[iSor + 2, iOszlop + 2];
+            for (int i = 0; i < iSor+2; i++)//tömbjeinket feltöltöttük.
+            {
+                for (int j = 0; j < iOszlop+2; j++)
+                {
+                    ia2Klikk[i, j] = 0;
+                    ia2Akna[i, j] = 0;
+                }
+            }
+            iMezo = iSor * iOszlop;
+            iAkna = iMezo / 10;
+            if (radioButton1.Checked)//hány aknánk van(zászlónk is van)
+            {
+                iAkna = iAkna * 1;
+            }
+            else if (radioButton2.Checked)
+            {
+                iAkna = iAkna * 2;
+            } else
+            {
+                iAkna = iAkna * 3;
+            }
+            label6.Text = iAkna.ToString();
+            for (int i = 0; i < iAkna; i++) //akna generálás
+            {
+                bool bSzabad = false;
+                while (!bSzabad)
+                {
+                    int iX, iY;
+                    iX = rnd.Next(1, iOszlop + 1);
+                    iY = rnd.Next(1, iSor + 1);
+                    if (ia2Akna[iY, iX]>-1)//nem akna
+                    {
+                        ia2Akna[iY, iX] = -1;
+                        bSzabad = true;
+                    }
+                }
+
+            }
+            for (int i = 1; i <= iSor; i++)
+            {
+                for (int j = 1; j <= iOszlop; j++)
+                {
+                    if (ia2Akna[i,j]!=-1)
+                    {//nem akna
+                        
+                        for (int k = -1; k <= 1; k++)
+                        {
+                            for (int l = -1; l <= 1; l++)
+                            {
+                                if (ia2Akna[i+k,j+l]==-1)//ha akna
+                                {
+                                    ia2Akna[i, j]++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            this.Invalidate(); //formot újra rajzoltatja(meghívja a paint eseményt)
+        }
+        /*minimum 5, maximum 20
+         * bekért számunk: bármi
+         *  Max(5, bekért számnak)-->bekért számunk nagyobb egyenlő mint 5
+         *Min(20, előző már átalaított számnak)
+         *Min(20, Max(5, bekért szám) )//
+         *
+         *
+         */
+
+        private void RacsKirajzol(Graphics rajzlap)
+        {
+            rajzlap.DrawRectangle(tollFeketeV,200, 200, 4+24*iOszlop,4+24*iSor );
+            for (int i = 1; i <= iSor; i++)
+            {
+                for (int j = 1; j <= iOszlop; j++)
+                {
+                    Rectangle recRacselem = new Rectangle(
+                        204+(j-1)*24,
+                        204+(i-1)*24,
+                        20,20);
+
+                    rajzlap.FillRectangle(ecsetFeher, recRacselem);
+                    rajzlap.DrawRectangle(tollFekete, recRacselem);
+                }
+            }
+        }
+
+     
+
+        private bool HovaKlikk(int iX, int iY)
+        {
+            bool bValos = false;
+            iX = iX - 204;
+            if (iX>=0)//nem balra klikkeltünk a rácstöl
+            {
+                int iTmpOszlop = iX / 24;
+                
+                iTmpOszlop += 1;
+                if (iTmpOszlop<=iOszlop)
+                {
+                    if (iX%24<=20)
+                    {
+                        bValos = true;
+                        iKoszlop = iTmpOszlop;
+                    }
+                }
+            }
+            
+            if (bValos)//valós oszlopszám
+            {
+                bValos = false;
+                iY = iY - 204;
+                if (iY >= 0)//nem balra klikkeltünk a rácstöl
+                {
+                    int iTmpSor = iY / 24;
+
+                    iTmpSor += 1;
+                    if (iTmpSor <= iSor)
+                    {
+                        if (iY % 24 <= 20)
+                        {
+                            bValos = true;
+                            iKsor = iTmpSor;
+                        }
+                    }
+                }
+            }
+
+            return bValos;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+           if( HovaKlikk(e.X, e.Y)&&!bVege&&bStart)//leklikk és rácsra klikeltünk-e?
+           {
+                if (ia2Klikk[iKsor,iKoszlop]!=0)// felvolt fedve
+                {
+                    if (e.Button==MouseButtons.Right)
+                    {
+                        if (ia2Klikk[iKsor,iKoszlop]==-1)//zászló??
+                        {
+                            ia2Klikk[iKsor, iKoszlop] = 0;
+                            iZaszlo--;//elvettem egy zászlót
+                        }
+                    }
+                } else
+                {
+                    if (e.Button==MouseButtons.Right)
+                    {
+                        if (iZaszlo<iAkna)//van még zászlónk
+                        {
+                            ia2Klikk[iKsor, iKoszlop] = -1;
+                            iZaszlo++;//letettem a zászlót
+                        }
+                        if (iFelforditott + iZaszlo == iMezo)
+                        {//ha minden mező felfordított vagy zászló akkor nyertünk
+                            bVege = false;
+                            MessageBox.Show("Győzelem, sikerült nyerni", "Győzelem",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+
+                    }
+                    if (e.Button == MouseButtons.Left) //bal klikk
+                    {
+                        if(ia2Akna[iKsor, iKoszlop] == -1)
+                        {
+                            bVege = true;
+                            MessageBox.Show("Vereség, akánára lépett", "Vereség",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        ia2Klikk[iKsor, iKoszlop] = 1;
+                        if (!bVege)
+                        {
+                            iFelforditott++;
+                        }
+                        if (iFelforditott+iZaszlo==iMezo)
+                        {//ha minden mező felfordított vagy zászló akkor nyertünk
+                            bVege = false;
+                            MessageBox.Show("Győzelem, sikerült nyerni", "Győzelem",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        
+                    }
+                    
+                }
+                this.Invalidate();
+
+           }
+        }
+
+        // ([(X-204)/24]+1)%24
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JatekKezdet();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (bStart)//ha elindult a jták
+            {
+                RacsKirajzol(e.Graphics);
+                label5.Text = iZaszlo.ToString();
+                for (int i = 1; i <= iSor; i++)
+                {
+                    for (int j = 1; j <= iOszlop; j++)
+                    {
+                        Rectangle recRacselem = new Rectangle(
+                        204 + (j - 1) * 24,
+                        204 + (i - 1) * 24,
+                        20, 20);
+                        if (ia2Klikk[i,j]==1)//felfordított
+                        {
+                            if (ia2Akna[i,j]==-1)
+                            {
+                                e.Graphics.FillEllipse(ecsetAkna,recRacselem);
+                            } else
+                            {
+                                e.Graphics.DrawString(
+                                    ia2Akna[i,j].ToString(),
+                                    fontKiiras,
+                                    ecsetBetu,recRacselem);
+                            }
+                        }
+                        else if (ia2Klikk[i,j]==-1)//zászló
+                        {
+                            Point[] pontazaszlo = new Point[3];
+                            pontazaszlo[0] = new Point(recRacselem.X+1,recRacselem.Y+7);
+                            pontazaszlo[1]= new Point(recRacselem.X + 17, recRacselem.Y + 3);
+                            pontazaszlo[2]= new Point(recRacselem.X + 17, recRacselem.Y + 10);
+                            e.Graphics.FillPolygon(ecsetAkna, pontazaszlo);
+                            e.Graphics.DrawLine(tollFeketeV, recRacselem.X + 17, recRacselem.Y + 3, recRacselem.X + 17, recRacselem.Y + 18);
+                        }
+                       
+                    }
+                }
+            }
+        }
+    }
+}
