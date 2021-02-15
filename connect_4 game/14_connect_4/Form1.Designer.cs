adacca7139bf349b06bde618d624cac6a631abd0
bazsycus <62937986+bazsycus@users.noreply.github.com>
2021-02-15 17:03:12 +0100

    Add files via upload

---
+++ connect_4 game/14_connect_4/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _14_connect_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 246);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Új játék";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 400);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 29);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "PvP";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 453);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 29);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "PvsAI";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 661);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximumSize = new System.Drawing.Size(1050, 700);
+            this.MinimumSize = new System.Drawing.Size(1050, 700);
+            this.Name = "Form1";
+            this.Text = "Connect 4";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ connect_4 game/14_connect_4/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14_connect_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //deklaráció
+        //int
+        int iOszlop;
+        int iSoronErtek;
+        // bool
+        bool bStart;
+        bool bGep = false;
+        
+        //ecsetek
+        SolidBrush ecsetTabla = new SolidBrush(Color.DarkBlue);
+        SolidBrush ecsetUresKorong = new SolidBrush(Color.Gray);
+        SolidBrush ecsetSarga = new SolidBrush(Color.Yellow);
+        SolidBrush ecsetPiros = new SolidBrush(Color.Red);
+        //tollka
+        //Random
+        Random rnd = new Random();
+        //tömb
+        int[,] ia2Tabla; //0 semmi, -1 piros, +1 sárga
+        private void Geplep()
+        {
+            if (Kinyert()==0)
+            {
+                int iMitlepagep = rnd.Next(0,7);
+                while (Sorszam(iMitlepagep)==-1)
+                {
+                    iMitlepagep = rnd.Next(0, 7);
+                }
+                ia2Tabla[iMitlepagep, Sorszam(iMitlepagep)] = -1;
+                iSoronErtek = 1;
+            }
+        }
+        private int Kinyert()
+        {
+            int iGyoztes = 0;
+
+            //összes sor
+            //összes oszlop
+            //összes átló
+            int iMellett = 0;
+            int iMelyik;
+            
+            
+            //a) összes oszlop
+
+            for (int i = 0; i < 7; i++)//végig minden oszlopon
+            {
+                iMellett = 0;//hány van egymás mellett
+                iMelyik = ia2Tabla[i, 5];//az aktuálisoszlop legalsó eleme
+                for (int j = 5; j >=0 ; j--)//aluról megyünk felfele
+                {//ha az adott elem nem nulla
+                    if (ia2Tabla[i,j]!=0)
+                    {
+                        //adott elem a vizsgált szín-e
+                        if (iMelyik==ia2Tabla[i,j])
+                        {
+                            iMellett++;//egyel több van egymás mellett
+                        } else
+                        {
+                            iMelyik = ia2Tabla[i, j];//legyen a másik szín
+                            iMellett = 1;
+                        }
+                        if (iMellett==4)
+                        {
+                            iGyoztes = iMelyik;
+                        }
+                    }
+
+                }
+            }
+
+            //b) összes sor
+
+            for (int j = 0; j < 6; j++)//végig minden soron
+            {
+                iMellett = 0;//hány van egymás mellett
+                iMelyik = ia2Tabla[0, j];//az aktuális sor legelső eleme
+                for (int i = 0; i < 7; i++)//balról jobbra
+                {//ha az adott elem nem nulla
+                    if (ia2Tabla[i, j] != 0)
+                    {
+                        //adott elem a vizsgált szín-e
+                        if (iMelyik == ia2Tabla[i, j])
+                        {
+                            iMellett++;//egyel több van egymás mellett
+                        }
+                        else
+                        {
+                            iMelyik = ia2Tabla[i, j];//legyen a másik szín
+                            iMellett = 1;
+                        }
+                        if (iMellett == 4)
+                        {
+                            iGyoztes = iMelyik;
+                        }
+                    }
+
+                }
+            }
+            //c) átló felső eleme balra le megy
+            for (int i = 3; i < 7; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    bool bNegyazonos = true;
+                    if (ia2Tabla[i,j]!=0)//nem-e üres
+                    {
+                        for (int k = 1; k <= 3; k++)
+                        {
+                            if (ia2Tabla[i,j]!=ia2Tabla[i-k,j+k])
+                            {
+                                bNegyazonos = false;
+                            }
+                        }
+                        if (bNegyazonos)
+                        {
+                            iGyoztes = ia2Tabla[i, j];
+                        }
+                    }
+                }
+            }
+            //d) átló felső eleme jobbra le megy
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    bool bNegyazonos = true;
+                    if (ia2Tabla[i, j] != 0)//nem-e üres
+                    {
+                        for (int k = 1; k <= 3; k++)
+                        {
+                            if (ia2Tabla[i, j] != ia2Tabla[i + k, j + k])
+                            {
+                                bNegyazonos = false;
+                            }
+                        }
+                        if (bNegyazonos)
+                        {
+                            iGyoztes = ia2Tabla[i, j];
+                        }
+                    }
+                }
+            }
+            return iGyoztes;
+        }
+
+        private bool Oszlopszam(int iX)
+        {
+            bool bSzabalyos = false;
+            iX -= 220; //keret, első korong határának levonása
+            if (iX>0)//nem a táblától balra
+            {
+                int iKOszlop = iX / 100;
+                if (iKOszlop<7)//7 oszlopunk van, nem-e jobbra klikk
+                {
+                    if (iX%100<80)
+                    {
+                        bSzabalyos = true;
+                        iOszlop = iKOszlop;
+                    }
+                }
+            }
+
+            return bSzabalyos;
+        }
+
+        private int Sorszam(int iOszlopszam)
+        {
+            int iSorszam = 5;//-1, ha nincs hely, különben hanyas sor
+            for (int i = 0; i < 6; i++)
+            {
+                iSorszam -= Math.Abs(ia2Tabla[iOszlopszam, i]);
+            }
+
+            return iSorszam;
+        }
+
+        private void Ujjatek()
+        {
+            bStart = true;
+            iOszlop = 0;
+            ia2Tabla = new int[7,6];//balról, jobbra, fentről, lefele
+            if (radioButton1.Checked)
+            {
+                bGep = false;
+                
+            } else
+            {
+                bGep = true;
+
+            }
+            if (rnd.Next(0, 2) == 1)
+            {
+                iSoronErtek = 1;
+            }
+            else
+            {
+                iSoronErtek = -1;//ha gépi, akkor ez a gép
+            }
+            if (bGep&&iSoronErtek==-1)
+            {
+                Geplep();
+            }
+
+            this.Invalidate();//kpernyő frissítés
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ujjatek();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (bStart)
+            {
+                if (Oszlopszam(e.X))
+                {
+                    if (Sorszam(iOszlop)>-1)
+                    {
+                        //ne a gép sora, 
+                        if ((bGep&&iSoronErtek==1)||!bGep)
+                        {//ez a gépesítéshez kell nem biztos hogy használjuk
+                            ia2Tabla[iOszlop, Sorszam(iOszlop)] = iSoronErtek;
+                            iSoronErtek = -iSoronErtek;
+                            if (bGep&&iSoronErtek==-1)//megvolt az emberi lépés
+                            {//jöhet a égpi lépés
+                                Geplep();
+                            }
+
+                        }
+                        
+                        this.Invalidate();
+                        if (Kinyert()!=0)
+                        {
+                            
+                            
+                            string sUzenet = "Nyert a";
+                            if (Kinyert()==1)
+                            {
+                                sUzenet += "sárga!";
+                            }
+                            else
+                            {
+                                sUzenet += "piros!";
+                            }
+                            MessageBox.Show(sUzenet, "Győzelem", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(ecsetPiros, 0, 0, 30, 30);
+            e.Graphics.FillRectangle(ecsetTabla, 200,20, 
+                20+7*100,20+6*100);
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    e.Graphics.FillEllipse(ecsetUresKorong,
+                        200 + 20 + i * 100, 20 + 20 + j * 100, 80, 80);
+                }
+            }
+
+            if (bStart)
+            {
+                if (iSoronErtek==1)
+                {
+                    e.Graphics.FillEllipse(ecsetSarga, 20, 20, 80, 80);
+                } else
+                {
+                    e.Graphics.FillEllipse(ecsetPiros, 20, 20, 80, 80);
+                }
+                for (int i = 0; i < 7; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if (ia2Tabla[i,j]==1)
+                        {
+                            e.Graphics.FillEllipse(ecsetSarga,
+                            200 + 20 + i * 100, 20 + 20 + j * 100, 80, 80);
+                        }
+                        else if (ia2Tabla[i,j]==-1)
+                        {
+                            e.Graphics.FillEllipse(ecsetPiros,
+                           200 + 20 + i * 100, 20 + 20 + j * 100, 80, 80);
+                        }
+                        
+                    }
+                }
+                if (Kinyert()!=0)
+                {
+                    bStart = false;
+                }
+            }
+
+        }
+    }
+}
